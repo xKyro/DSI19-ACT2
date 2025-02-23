bdae4cb944c91751ff341958d34e95e067b728dc
kyro <kyrodaboi@gmail.com>
2025-02-23 14:59:42 -0500

    test

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,6 @@
 function redirect(path){
     const currentDomain = window.location.origin;
 
-    window.location.assign(`${currentDomain}/${path}`);
+    // window.location.assign(`${currentDomain}/${path}`);
+    console.log(window.location);
 }
